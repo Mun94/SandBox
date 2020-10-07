11742ba989b0b341a3fe59550b3396bb9c8588bf
Mun94 <seongwoo720@gmail.com>
2020-10-08 07:47:01 +0900

    201008 | DONE | ChannelInfo 크리에이터 이름 디자인

--- sb_front/src/components/channelInfo/ChannelInfo.js
+++ sb_front/src/components/channelInfo/ChannelInfo.js
@@ -34,13 +34,109 @@ const NameSubs = styled.div`
   div:nth-child(1) {
     font-size: 1.3rem;
     text-align: center;
+    position: relative;
+    overflow: hidden;
+    padding: 10px 20px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5);
+    margin-bottom: 1rem;
+  }
+  div:nth-child(1):before {
+    content: '';
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    bottom: 2px;
+    width: 50%;
+    background: rgba(255, 255, 255, 0.05);
   }
   div:nth-child(1):hover {
-    background: #ffc200;
-    border-radius: 4px;
+    color: #ffc200;
   }
   div:nth-child(2) {
     text-align: center;
+  }
+
+  a span:nth-child(1) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(to right, blue, pink);
+    /*line 생성*/
+
+    animation: animate1 1.5s linear infinite; /* 에니메이션 */
+  }
+
+  @keyframes animate1 {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(100%);
+    }
+  } /*에니메이션 조건*/
+
+  a span:nth-child(2) {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(to bottom, blue, pink);
+
+    animation: animate2 1.5s linear infinite;
+    animation-delay: 0.8s;
+  }
+
+  @keyframes animate2 {
+    0% {
+      transform: translateY(-100%);
+    }
+    100% {
+      transform: translateY(100%);
+    }
+  }
+
+  a span:nth-child(3) {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(to left, blue, pink);
+
+    animation: animate3 1.5s linear infinite;
+  }
+
+  @keyframes animate3 {
+    0% {
+      transform: translateX(100%);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+
+  a span:nth-child(4) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(to top, blue, pink);
+
+    animation: animate4 1.5s linear infinite;
+    animation-delay: 0.8s;
+  }
+
+  @keyframes animate4 {
+    0% {
+      transform: translateY(100%);
+    }
+    100% {
+      transform: translateY(-100%);
+    }
   }
 `;
 
@@ -60,7 +156,13 @@ const ChannelInfo = ({ channelInfo, channelId }) => {
                 <img src={ch.profileUrl} alt="" />
                 <NameSubs>
                   <Link to={routes.videos(ch.channelId)}>
-                    <div>{ch.name}</div>
+                    <div>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      {ch.name}
+                    </div>
                   </Link>
                   <div>{ch.subs}명</div>
                 </NameSubs>
